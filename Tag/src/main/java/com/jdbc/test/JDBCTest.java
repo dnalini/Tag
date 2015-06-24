4f4fce09d1677a4b8cf5b725c5f01f76db8f5d5d
nvemuri <nalinikumari.vemuri@tagworldwide.com>
2015-06-24 15:23:37 +0530

    JDBC example

---
+++ Tag/src/main/java/com/jdbc/test/JDBCTest.java
@@ -0,0 +1,28 @@
+package com.jdbc.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class JDBCTest {
+	
+	public static void main(String[] args) {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/dbname","test","test");
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println();
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
